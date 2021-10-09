09055e35288ab980263d93ebd48828847803193b
ダイゴロー <ft480110@gmail.com>
2021-10-09 16:45:37 +0900

    ・jquery,bootstrapが適用されるよう修正。3

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,16 @@
+$(function () {
+    var password = '#password';
+    var checkPassword = '#displayPassword';
+
+    $(checkPassword).change(function () {
+        if ($(this).prop('checked')) {
+            $(password).attr('type', 'text');
+        } else {
+            $(password).attr('type', 'password');
+        }
+    });
+});
+
 function dialogHide() {
     var dialog = document.getElementById("dialog");
     dialog.style.display = "none";
