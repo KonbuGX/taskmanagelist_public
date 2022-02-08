6324272ec2da3d3c8fa710c16e3ced18168ef319
ダイゴロー <ft480110@gmail.com>
2022-02-08 19:38:20 +0900

    リファクタリング ・jsのエラーが出ていたので修正。 ・cssファイルにてidやクラスに対しコメントを追加。 ・ハンバーガーメニューの不要なidやクラスなどを削除。

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,50 +13,71 @@ $(function () {
 
     /*PCとその他デバイスでのボタン押下時のデザイン処理*/
     var user = navigator.userAgent;
-    var dialogOkButton = '.dialogOkButton';
-    if(user.indexOf("iPhone") > 0 || user.indexOf("iPad") > 0 || user.indexOf("Android") > 0){
-        $('button').bind("touchstart", function () {
-			$(this).addClass("hover");
-            dialogOkButton.addClass("hover");
-		}).bind("touchend", function () {
-			$(this).removeClass("hover");
-            dialogOkButton.removeClass("hover");
-		});
-    }else{
-        $('button').hover(
-			function () {
-				$(this).addClass("hover");
-                dialogOkButton.addClass("hover");
-			},
-			function () {
-				$(this).removeClass("hover");
-                dialogOkButton.removeClass("hover");
-			}
-		);
-    }
+    var dialog = document.getElementById("dialog");
+        if(user.indexOf("iPhone") > 0 || user.indexOf("iPad") > 0 || user.indexOf("Android") > 0){
+            $('button').bind("touchstart", function () {
+                $(this).addClass("hover");
+            }).bind("touchend", function () {
+                $(this).removeClass("hover");
+            });
+
+            //アカウント削除ボタン押下時時のダイアログが表示されている時のみ
+            if(dialog != null && dialog.style.display != "none"){
+                $('.dialogOkButton').bind("touchstart", function () {
+                    $(this).addClass("hover");
+                }).bind("touchend", function () {
+                    $(this).removeClass("hover");
+                });
+            }
+        }else{
+            $('button').hover(
+                function () {
+                    $(this).addClass("hover");
+                },
+                function () {
+                    $(this).removeClass("hover");
+                }
+            );
+
+            //アカウント削除ボタン押下時時のダイアログが表示されている時のみ
+            if(dialog != null && dialog.style.display != "none"){
+                $('.dialogOkButton').hover(
+                    function () {
+                        $(this).addClass("hover");
+                    },
+                    function () {
+                        $(this).removeClass("hover");
+                    }
+                );
+            }
+        }
 
     //期日によるタスク行の背景色変更
+    //ホーム画面のタスクリストのみ処理を実行
     let taskTable = document.getElementById("taskTable");
-    var tempDate = new Date();
-    var today = Date.parse(tempDate.getFullYear()+"-"+(tempDate.getMonth()+1)+"-"+tempDate.getDate());
-    for(let row of taskTable.rows){
-        //ヘッダーは処理の対象外
-        if(taskTable.rows[0] == row){
-            continue;
-        }
-
-        var status = row.cells[4].innerText;
-        var deadline = Date.parse(row.cells[3].innerText);
-        if(status != "完了" && deadline < today){
-            for(let cell of row.cells){
-                cell.style.backgroundColor = "rgb(255, 130, 130)";
+    if(taskTable != null && taskTable.rows[0].cells.length > 4){
+        var tempDate = new Date();
+        var today = Date.parse(tempDate.getFullYear()+"-"+(tempDate.getMonth()+1)+"-"+tempDate.getDate());
+        for(let row of taskTable.rows){
+            //ヘッダーは処理の対象外
+            if(taskTable.rows[0] == row){
+                continue;
             }
-        }else if(status == "完了"){
-            for(let cell of row.cells){
-                cell.style.backgroundColor = "rgb(204, 204, 204)";
+
+            var status = row.cells[4].innerText;
+            var deadline = Date.parse(row.cells[3].innerText);
+            if(status != "完了" && deadline < today){
+                for(let cell of row.cells){
+                    cell.style.backgroundColor = "rgb(255, 130, 130)";
+                }
+            }else if(status == "完了"){
+                for(let cell of row.cells){
+                    cell.style.backgroundColor = "rgb(204, 204, 204)";
+                }
             }
         }
     }
+    
 });
 
 //ハンバーガーメニューの表示非表示の処理
@@ -64,11 +85,9 @@ function hamburgerMenuShow(){
     if ($('.nav-btn').hasClass('open')) {
         $('.nav-btn').removeClass('open');
         $('#navi').removeClass('open-menu');
-        $('#navi-memo').removeClass('open-menu');
     } else {
         $('.nav-btn').addClass('open');
         $('#navi').addClass('open-menu');
-        $('#navi-memo').addClass('open-menu');
     }
 }
 
@@ -97,6 +116,7 @@ function switchShowList() {
     return;
 }
 
+//メモ画面の編集、削除ボタン押下時のダイアログ表示処理
 function taskMemoDialogShow(id,memoNo,status) {
     let msg = $('a#'+id).parent('.memoTableBtn').prev('td.memoTableTd').text();
     if(status == "編集"){
@@ -113,6 +133,7 @@ function taskMemoDialogShow(id,memoNo,status) {
     return;
 }
 
+//メモ画面のダイアログ非表示表示処理
 function taskMemoDialogHide(status) {
     if(status == "編集"){
         var dialog = document.getElementById("updateMemoDialog");
